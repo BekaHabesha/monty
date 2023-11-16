f9ba092f8fad5cd523f4ead33101c55fffb4ce03
BekaHabesha <bereketdm1984@gmail.com>
2023-11-16 09:05:56 -0800

    f_pchar.c - 0x19.C-Stacks, Queues-LIFO, FIFO f_pchar.c file Authored by Bereket Dereje

---
+++ f_pchar.c
@@ -0,0 +1,48 @@
+/*
+* File_name: f_pchar.c file
+* Functions: void f_pchar
+* Created: On November 16, 2023
+* Author: Bereket Dereje Mekonnen
+* GitHub repository: monty
+* Project: 0x19.C-Stacks, Queues-LIFO, FIFO
+* Directory: monty
+*/
+
+#include "monty.h"
+
+/**
+ * f_pchar - It prints char at top of the stack,
+ *           followed by a new line
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: The stack head
+ *
+ * @counter: The line_number
+ *
+ * Return: no return
+ */
+
+void f_pchar(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	h = *head;
+	if (!h)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if (h->n > 127 || h->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", h->n);
+}
