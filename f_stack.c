1a0fa340d85be7df01560f77e1fe6091b9ba9cbd
BekaHabesha <bereketdm1984@gmail.com>
2023-11-16 09:45:05 -0800

    f_stack.c - 0x19.C-Stacks, Queues-LIFO, FIFO f_stack.c file Authored by Bereket Dereje

---
+++ f_stack.c
@@ -0,0 +1,30 @@
+/*
+* File_name: f_stack.c file
+* Functions: void f_stack
+* Created: On November 16, 2023
+* Author: Bereket Dereje Mekonnen
+* GitHub repository: monty
+* Project: 0x19.C-Stacks, Queues-LIFO, FIFO
+* Directory: monty
+*/
+
+#include "monty.h"
+
+/**
+ * f_stack - It prints the top
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: The stack head
+ *
+ * @counter: The line_number
+ *
+ * Return: no return
+ */
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
