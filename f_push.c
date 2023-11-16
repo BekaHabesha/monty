385ff3905bc7eb5c391b595fd0580ffbdbd4bc69
BekaHabesha <bereketdm1984@gmail.com>
2023-11-16 09:21:50 -0800

    f_push.c - 0x19.C-Stacks, Queues-LIFO, FIFO f_push.c file Authored by Bereket Dereje

---
+++ f_push.c
@@ -0,0 +1,57 @@
+/*
+* File_name: f_push.c file
+* Functions: void f_push
+* Created: On November 16, 2023
+* Author: Bereket Dereje Mekonnen
+* GitHub repository: monty
+* Project: 0x19.C-Stacks, Queues-LIFO, FIFO
+* Directory: monty
+*/
+
+#include "monty.h"
+
+/**
+ * f_push - It add node to the stack
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @head: The stack head
+ *
+ * @counter: The line_number
+ *
+ * Return: no return
+ */
+
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, j, flag;
+
+	int j = 0;
+	int flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
+}
