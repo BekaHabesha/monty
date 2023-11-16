a528b638486e703480c5d3215ea08c86ffc74d04
BekaHabesha <bereketdm1984@gmail.com>
2023-11-16 12:34:28 -0800

    monty - 0x19.C-Stacks, Queues-LIFO, FIFO monty file Authored by Bereket Dereje

--- monty_main.c
+++ monty_main.c
@@ -9,11 +9,12 @@
 */
 
 #include "monty.h"
-#define _GNU_SOURCE
 #include <stdio.h>
-#define  _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
+#include <stdlib.h>
 
 bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
  * main - The function for monty code interpreter
  *
@@ -25,11 +26,11 @@ bus_t bus = {NULL, NULL, NULL, 0};
  *
  * Return: 0 on success
  */
+
 int main(int argc, char *argv[])
 {
 	char *content;
 	FILE *file;
-	size_t size = 0;
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
@@ -49,9 +50,7 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
+		
 		if (read_line > 0)
 		{
 			execute(content, &stack, counter, file);
