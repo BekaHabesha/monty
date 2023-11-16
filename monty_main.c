a1279ee3e23b7a69e9da7ccbf0a0171173793965
BekaHabesha <bereketdm1984@gmail.com>
2023-11-16 07:01:41 -0800

    monty_main.c - 0x19.C-Stacks, Queues-LIFO, FIFO monty_main.c file Authored by Bereket Dereje

---
+++ monty_main.c
@@ -0,0 +1,71 @@
+/*
+* File_name: monty_main.c file
+* Functions: int main
+* Created: On November 16, 2023
+* Author: Bereket Dereje Mekonnen
+* GitHub repository: monty
+* Project: 0x19.C-Stacks, Queues-LIFO, FIFO
+* Directory: monty
+*/
+
+#include "monty.h"
+#include <stdio.h>
+#define _GNU_SOURCE
+#include <stdlib.h>
+
+bus_t bus = {NULL, NULL, NULL, 0};
+
+/**
+ * main - The function for monty code interpreter
+ *
+ * Author: Bereket Dereje Mekonnen
+ *
+ * @argc: The number of arguments
+ *
+ * @argv: The monty file location
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	char *content;
+	FILE *file;
+	size_t size;
+	ssize_t read_line;
+	stack_t *stack;
+	unsigned int counter;
+
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
+}
